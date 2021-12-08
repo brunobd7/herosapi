4ed1c61bab9d69cc33b37af52448a85cdea0422a
Bruno Dantas <brunoodantas7@gmail.com>
2021-12-08 00:19:19 -0300

    Initial Commit

---
+++ src/test/java/com/dio/herosapi/HerosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.herosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerosapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
